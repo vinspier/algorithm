a24a6294723337b5e2f5ea08baee9cc7a3939298
vinspier <954579147@qq.com>
2020-03-18 12:00:51 +0800

    编写趣味21点游戏

---
+++ delight-algorithm/src/com/vinspier/TwentyOne/TestTwentyOne.java
@@ -0,0 +1,20 @@
+package com.vinspier.TwentyOne;
+
+/**
+ * @ClassName: TestTwentyOne
+ * @Description: 21点游戏
+ * @Author:
+ * @Date: 2020/3/17 10:22
+ * @Version V1.0
+ **/
+
+public class TestTwentyOne {
+    public static void main(String[] args) {
+        TwentyOne twentyOne = new TwentyOne(10,24);
+        twentyOne.bet(twentyOne.getAmount(),0);
+        twentyOne.getLeftData().stream().forEach((i)-> System.out.println(i));
+        System.out.println(twentyOne.getAvaliable());
+    }
+
+
+}
